ac82b42ee90c8103f79625961a58c1492c0af246
Ashwanth770 <ashwanths.8546.sssmsc@gmail.com>
2025-10-30 09:59:41 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.set_page_config(page_title="AI Portfolio Builder", layout="wide")
+st.set_page_config(page_title="AI-Based Personal Website Builder", layout="wide")
 
-st.title("📋 AI-Based Portfolio Generator")
+st.title("📋 AI-Based Personal Website Builder")
 st.markdown("""
 Welcome to your personal AI-powered portfolio builder.
 
@@ -11,3 +11,4 @@ Use the sidebar to:
 - Generate your HTML website
 - Download a zip
 - View preview of your site """)
+
